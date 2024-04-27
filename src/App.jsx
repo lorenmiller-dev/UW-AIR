16b546cff85d46ae2d7ebefe3370d43e89a25c39
loren <lorenmiller.dev@gmail.com>
2024-04-26 21:10:00 -0700

    Basic folder structure setup and testing logo ideas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/airuw.svg'
 import './App.css'
 
 function App() {
@@ -12,11 +12,8 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='bg-red-500 text-black'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
